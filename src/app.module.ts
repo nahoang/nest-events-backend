6e6e3a5090f26baf4efb20b4b3a2b054a86b3cc9
nahoang <nahoang9698@gmail.com>
2023-02-15 14:54:59 +0700

    feat: Implement Event moduel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Event } from './event.entity';
 import { EventsController } from './events.controller';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3307,
+    username: 'root',
+    password: 'example',
+    database: 'nest-events',
+    entities: [Event],
+    synchronize: true,
+  }),
+  TypeOrmModule.forFeature([Event])
+],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
